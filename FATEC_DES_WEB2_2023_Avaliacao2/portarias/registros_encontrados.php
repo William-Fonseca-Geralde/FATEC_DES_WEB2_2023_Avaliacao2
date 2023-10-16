0b2b1b9ab06f035cc5df05a6ea4baa8c7a44cb8a
William-Fonseca-Geralde <127157689+William-Fonseca-Geralde@users.noreply.github.com>
2023-10-16 16:10:35 -0300

    commit

--- FATEC_DES_WEB2_2023_Avaliacao2/portarias/registros_encontrados.php
+++ FATEC_DES_WEB2_2023_Avaliacao2/portarias/registros_encontrados.php
@@ -40,14 +40,9 @@
             <label>Data e Hora em que existe registro de entrada/saída</label>
             <br>
             <?php
-                if (!isset($row['data_hora'])) {
-                    echo "não há registro de hora";
-                } else {
-                    while ($row = $stmt->fetch()) {
-                        echo $row['data_hora'] . " / ";
-                    }
+                while ($row = $stmt->fetch()) {
+                    echo $row['data_hora'] . " / ";
                 }
-                
                 $conn = NULL;
             ?>
         </div>
